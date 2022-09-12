57be722f6c12e0aefa4c937d35d968e89545081c
jrpp_199 <jrpp198@gmail.com>
2022-09-12 13:50:57 -0300

    EvoCarDetail

---
+++ script.js
@@ -0,0 +1,8 @@
+const extencibles = document.getElementsByClassName('section__card');
+
+for (let index = 0; index < 2; index++) {
+    const element = extencibles[index].getElementsByClassName('card__text');
+}
+
+
+console.log(extencibles);
